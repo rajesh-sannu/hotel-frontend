9523a1bf9a0804f350e334812de6ace52518065e
venkata rajesh <sannurajesh1057@gmail.com>
2025-11-29 13:37:35 +0530

    fix: updated axios baseURL to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,15 @@
-// src/api/axios.js
+/* src/api/axios.js
 import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:5000/api",
+});*/
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://hotel-backend-2-g911.onrender.com/api",
 });
+
+export default API;
+
